9fb0bd2ed60875e4add38477a8bc12a3160f7e00
Pruthvij008 <121214685+Pruthvij008@users.noreply.github.com>
2024-10-22 17:47:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173"||"https://imagestopdfnew-1.onrender.com/",
+    origin:"https://imagestopdfnew-1.onrender.com/",
   })
 );
 
